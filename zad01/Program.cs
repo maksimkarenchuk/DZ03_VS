17e2487d8effead9eac2c3da4c267ac4e1f2f51f
max <mail.com>
2022-08-27 19:45:11 +0300

    палиндром

---
+++ zad01/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+public class IsPalindrome
+{
+public static void Main(string[] args)
+{
+    int no,total=0,t,rev;
+    Console.Write("Введите число ");
+    no = int.Parse(Console.ReadLine());
+    t=no;
+        while(no > 0)
+        {
+                rev = no % 10;
+                total = (total * 10) + rev;
+                no = no / 10;
+        }
+        if(t == total)
+                Console.Write("Это число палиндром");
+        else
+                Console.Write("Это число не палиндром");
+        }
+        }
