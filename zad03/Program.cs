f8f9d8676c156f4fdfbfbea9c3cdb08af9165b9c
max <mail.com>
2022-08-27 19:47:03 +0300

    куб числа

---
+++ zad03/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+
+
+string numderskub()
+ {
+    Console.Write("Введите положительное число ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int i=1;
+            string res = string.Empty;   //пустая строка
+            while (i <= N)
+            {
+                res = res + i*i*i+", ";
+                i=i+1;
+            }
+            return res;
+ }
+ 
+ Console.WriteLine(numderskub());
